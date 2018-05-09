d6415f8798974449dbc8ed9347288e122ba3e974
Peter Evans <peterevans@engageinlearning.com>
2018-05-09 15:19:15 +0100

    Added file upload and xlsx reader modules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dropzone from 'react-dropzone'
+let dropzoneRef;
+import * as XLSX from 'xlsx';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = { files: [] }
+  }
+
+  onDrop(files) {
+    this.setState({
+      files
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +26,27 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <section>
+          <div className="dropzone">
+            <Dropzone ref={(node) => { dropzoneRef = node; }} onDrop={this.onDrop.bind(this)}>
+              <p>Try dropping some files here, or click to select files to upload.</p>
+            </Dropzone>
+              <button type="button" onClick={() => { dropzoneRef.open() }}>
+                Open File Dialog
+              </button>
+          </div>
+          <aside>
+            <h2>Dropped files</h2>
+            <ul>
+              {
+                this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+              }
+            </ul>
+          </aside>
+        </section>
+
+        {{ console.log(this.state.files.length) }}
       </div>
     );
   }
